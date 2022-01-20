2eb920cc4b2277374e612567c3580effe4f3febe
= <danishfaisal.wwe@gmail.com>
2022-01-20 22:35:03 +0530

    Creating and linking the articles list

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlesList = () => {
     return (
         <>
             <h1>Articles List</h1>
+            {
+                articles.map((article, idx) => {
+                    return (
+                        <Link className="article-list-item" key={idx} to={`/article/${article.name}`}>
+                            <h3>{article.title}</h3>
+                            <p>{article.content[0].substring(0, 150)}...</p>
+                        </Link>
+                    );
+                })
+            }
         </>
     );
 }
