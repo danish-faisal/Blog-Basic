4dae6dd90d99be226459f709b456b6156d9bd90a
= <danishfaisal.wwe@gmail.com>
2022-01-20 22:04:36 +0530

    URL parameters with react-router

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,7 +1,22 @@
-const ArticlePage = () => {
+import { useParams } from "react-router";
+import articles from "./article-content";
+
+const ArticlePage = ({ match }) => {
+    const { name } = useParams();
+
+    const article = articles.find(article => article.name === name);
+
+    if (!article) return <h1>Article does not exist!</h1>;
+
     return (
         <>
-            <h1>This is an article</h1>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, idx) => {
+                return (
+                    <p key={idx}>{paragraph}</p>
+                );
+            })
+            }
         </>
     );
 }
