16b5d15a89401969feb9c95ea45ac53f101c83ee
= <danishfaisal.wwe@gmail.com>
2022-01-22 22:20:33 +0530

    Using React Hooks

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import ArticlesList from "../components/ArticlesList";
 import articles from "./article-content";
@@ -8,6 +9,12 @@ const ArticlePage = () => {
 
     const article = articles.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3 });
+    });
+
     if (!article) return <NotFoundPage />;
 
     const otherArticles = articles.filter(article => article.name !== name);
@@ -15,6 +22,7 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times</p>
             {article.content.map((paragraph, idx) => {
                 return (
                     <p key={idx}>{paragraph}</p>
